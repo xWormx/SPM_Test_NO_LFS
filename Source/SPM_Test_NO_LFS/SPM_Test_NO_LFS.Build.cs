d1fcd92265e1b1f91147fb4a62c4b5bb701b02cc
xWormx <carl_johan6@hotmail.com>
2025-04-08 20:27:19 +0200

    Niagara i build

--- Source/SPM_Test_NO_LFS/SPM_Test_NO_LFS.Build.cs
+++ Source/SPM_Test_NO_LFS/SPM_Test_NO_LFS.Build.cs
@@ -8,7 +8,7 @@ public class SPM_Test_NO_LFS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
