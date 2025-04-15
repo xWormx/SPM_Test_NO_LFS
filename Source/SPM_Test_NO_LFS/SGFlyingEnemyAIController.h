e08b36a794120aa8476cb34492079481b73520a9
basirghani <144438561+basirghani@users.noreply.github.com>
2025-04-15 18:48:00 +0200

    Skapade FlyingEnemyAIController

---
+++ Source/SPM_Test_NO_LFS/SGFlyingEnemyAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SGFlyingEnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPM_TEST_NO_LFS_API ASGFlyingEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
