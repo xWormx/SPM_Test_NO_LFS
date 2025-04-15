04a638ec2f39ee7d87d8f19cff6ddcb0f644b439
basirghani <144438561+basirghani@users.noreply.github.com>
2025-04-15 18:18:31 +0200

    La till FlyingEnemyClass

---
+++ Source/SPM_Test_NO_LFS/SGFlyingEnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SGEnemyCharacter.h"
+#include "SGFlyingEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPM_TEST_NO_LFS_API ASGFlyingEnemyCharacter : public ASGEnemyCharacter
+{
+	GENERATED_BODY()
+	
+};
