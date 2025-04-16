2656bf140bcb97f5080e5a13797f49e190da5c8f
Emma <86224710+emrila@users.noreply.github.com>
2025-04-16 19:40:20 +0200

    Lagt till: EnemyDropManager (C++ & Blueprint)
    WIP

---
+++ Source/SPM_Test_NO_LFS/SGEnemyDropManager.cpp
@@ -0,0 +1,57 @@
+﻿#include "SGEnemyDropManager.h"
+
+#include "SGEnemyCharacter.h"
+#include "SGPickUp.h"
+
+ASGEnemyDropManager::ASGEnemyDropManager()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ASGEnemyDropManager::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+void ASGEnemyDropManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ASGEnemyDropManager::DropItem(ASGEnemyCharacter* EnemyCharacter)
+{
+	if (!EnemyCharacter)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("EnemyCharacter is null."));
+		return;
+	}
+
+	const FSGEnemyDropContainer* Drops = EnemyDropMap.Find(EnemyCharacter->GetClass());
+
+	if (!Drops)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No drops found for the given enemy."));
+		return;
+	}
+
+	for (const auto& Drop : Drops->EnemyDrops)
+	{
+		if (!Drop.PickUpClass)
+		{
+			continue;
+		}
+		
+		FVector SpawnLocation = EnemyCharacter->GetActorLocation();
+		FRotator SpawnRotation = FRotator::ZeroRotator;
+		
+		for (int i = 0; i < Drop.PickUpCount; ++i)
+		{
+			//Den bortkommenterade koden är inte ordentligt testad än
+			/* FVector RandomOffset = FMath::VRand() * FMath::RandRange(0.0f, Drop.SpawnRadius);
+			FVector RandomSpawnLocation = SpawnLocation + RandomOffset;
+			GetWorld()->SpawnActor<ASGPickUp>(Drop.PickUpClass, RandomSpawnLocation, SpawnRotation);*/
+			GetWorld()->SpawnActor<ASGPickUp>(Drop.PickUpClass, SpawnLocation, SpawnRotation);
+		}
+	}
+}
+
---
+++ Source/SPM_Test_NO_LFS/SGEnemyDropManager.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Containers/Map.h"
+#include "SGEnemyDropManager.generated.h"
+
+class ASGPickUp;
+
+//För att smidigare ange vilken typ av fiende som ska droppa vad, mängd och potentiell spawnradius om flera av samma typ ska spawna
+USTRUCT(BlueprintType, Category = "Item Drop")
+struct FSGEnemyDrop
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UProperty - PickUp Type", meta = (ToolTip = "Använd motsvarande Blueprint-klass för att spawna"))
+	TSubclassOf<ASGPickUp> PickUpClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UProperty - PickUp Count")
+	int PickUpCount = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UProperty - PickUp Count")
+	float SpawnRadius = 10.0f;
+};
+
+//Workaround för att kunna ha TArray i en TMap 
+USTRUCT(BlueprintType, Category = "Item Drop")
+struct FSGEnemyDropContainer
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UProperty - Enemy Drops")
+	TArray<FSGEnemyDrop> EnemyDrops;
+};
+
+class ASGEnemyCharacter;
+
+UCLASS()
+class SPM_TEST_NO_LFS_API ASGEnemyDropManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASGEnemyDropManager();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	//Anropas när fienden dör
+	UFUNCTION(BlueprintCallable, Category = "UFunction - Drop Item")
+	void DropItem(ASGEnemyCharacter* EnemyCharacter);
+
+private:
+	
+	//TODO: Om tid och värt - använd data table istället
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UProperty - PickUp Type", meta = (AllowPrivateAccess = "true", ToolTip = "Använd motsvarande Blueprint-klass som nyckel"))
+	TMap<TSubclassOf<ASGEnemyCharacter>, FSGEnemyDropContainer> EnemyDropMap;
+};
