19ed0f5c42278456e435e07721ea93f6c42f60b0
Emma <emmarichterlarsson@gmail.com>
2025-04-24 15:37:52 +0200

    ADD - PickUpObjectiveCollect (C++ och BP)

---
+++ Source/SPM_Test_NO_LFS/SGPickUpObjectiveCollect.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SGPickUp.h"
+#include "SGPickUpObjectiveCollect.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCollected, ASGPickUpObjectiveCollect*, Collectable);
+
+UCLASS()
+class SPM_TEST_NO_LFS_API ASGPickUpObjectiveCollect : public ASGPickUp
+{
+	GENERATED_BODY()
+
+public:
+	ASGPickUpObjectiveCollect();
+	FOnCollected OnCollected;
+
+	virtual void OnPickup_Implementation() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void HandleOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	
+	UPROPERTY(EditAnywhere, Category = "UPROPERTY - PickUp")
+	class UStaticMeshComponent* Mesh;
+};
