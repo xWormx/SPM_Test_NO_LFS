37596ff3288504b58baa043928f60ab8af8270b7
Emma <emmarichterlarsson@gmail.com>
2025-04-28 16:55:02 +0200

    ADD - Välja datatabellen för uppgradering via custom sektion i Project Settings
    Något att utvärdera. Själva Uppgraderingssystemet (som är ett subsystem) hämtar datatabellen därifrån.

---
+++ Source/SPM_SGUpgradeSystem/Public/SGUpgradeDeveloperSettings.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "SGUpgradeDeveloperSettings.generated.h"
+
+UCLASS(config=Game, DefaultConfig)
+class SPM_SGUPGRADESYSTEM_API USGUpgradeDeveloperSettings : public UDeveloperSettings
+{	
+	GENERATED_BODY()
+public:
+
+	virtual FName GetCategoryName() const override;
+	
+#if WITH_EDITOR
+	virtual FText GetSectionText() const override;
+	virtual FText GetSectionDescription() const override;
+#endif
+
+	UPROPERTY(Config, EditAnywhere, Category="Upgrades Data Table", meta=(AllowedClasses="/Script/Engine.DataTable"))
+	FSoftObjectPath UpgradeDataTable;		
+};
