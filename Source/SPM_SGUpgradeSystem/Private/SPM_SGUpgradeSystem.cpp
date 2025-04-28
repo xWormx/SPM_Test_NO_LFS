0aaffd42e9318c5e61666ffebbca9a354104534d
Emma <emmarichterlarsson@gmail.com>
2025-04-28 14:34:48 +0200

    ADD - Modul för uppgraderingssystemet
    Kortfattat översikt:
    - SGUpgradeSystem.*: "modulklassen"
    - SGUpgradeSystem.Build.cs: modulens dependencies
    - *.Target.cs: "Registrerar" modulens existens
    - *.uproject: Gör modulen basically synlig i editorn

---
+++ Source/SPM_SGUpgradeSystem/Private/SPM_SGUpgradeSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "SPM_SGUpgradeSystem.h"
+
+#define LOCTEXT_NAMESPACE "FSPM_SGUpgradeSystemModule"
+
+void FSPM_SGUpgradeSystemModule::StartupModule()
+{
+    
+}
+
+void FSPM_SGUpgradeSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FSPM_SGUpgradeSystemModule, SPM_SGUpgradeSystem)
