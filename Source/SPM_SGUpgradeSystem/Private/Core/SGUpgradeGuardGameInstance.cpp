086c7e46a3eb0e7730f69f86e09b2b78e036d4f4
Emma <emmarichterlarsson@gmail.com>
2025-04-30 20:11:05 +0200

    ADD - (Förmodligen temp) GI som hanterar om man får uppdatera

---
+++ Source/SPM_SGUpgradeSystem/Private/Core/SGUpgradeGuardGameInstance.cpp
@@ -0,0 +1,21 @@
+﻿#include "Core/SGUpgradeGuardGameInstance.h"
+
+void USGUpgradeGuardGameInstance::AddToCount(float Amount)
+{
+	Count += Amount;
+}
+
+void USGUpgradeGuardGameInstance::RemoveFromCount(float Amount)
+{
+	Count -= Amount;
+}
+
+bool USGUpgradeGuardGameInstance::CanUpgradeBasedOnCount(const float UpgradeCost) const
+{
+	return Count - UpgradeCost >= 0;
+}
+
+float USGUpgradeGuardGameInstance::GetCount() const
+{
+	return Count;
+}
