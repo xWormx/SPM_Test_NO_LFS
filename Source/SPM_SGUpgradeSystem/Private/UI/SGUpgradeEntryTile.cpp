3b77778daca7ef953f96b222ec7541136b87de30
Emma <emmarichterlarsson@gmail.com>
2025-04-30 18:36:40 +0200

    ADD - (Widget) USGUpgradeEntryTile (C++ & BP)
    Container för en attribut

---
+++ Source/SPM_SGUpgradeSystem/Private/UI/SGUpgradeEntryTile.cpp
@@ -0,0 +1,43 @@
+﻿#include "UI/SGUpgradeEntryTile.h"
+
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "Core/SGAttribute.h"
+#include "Core/SGUpgradeSubsystem.h"
+
+void USGUpgradeEntryTile::SetupEntry(const FSGUpgradeEntry& Entry)
+{
+	BoundRowName = Entry.RowName;
+
+	if (Icon && Entry.Icon)
+	{
+		Icon->SetBrushFromTexture(Entry.Icon);
+	}
+
+	if (UpgradeText)
+	{
+		const FText EntryCost = FText::AsNumber(Entry.Cost);
+		const FText EntryMultiplier = FText::AsNumber(Entry.Multiplier);	
+		UpgradeText->SetText(FText::Format(FText::FromString("Cost: {0} Multiplier: {1}"), EntryCost, EntryMultiplier));
+	}	
+}
+
+void USGUpgradeEntryTile::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (UpgradeButton)
+	{
+		UpgradeButton->OnClicked.AddDynamic(this, &USGUpgradeEntryTile::HandleClicked);
+	}
+}
+
+void USGUpgradeEntryTile::HandleClicked()
+{
+	USGUpgradeSubsystem* UpgradeSubsystem = GetGameInstance()->GetSubsystem<USGUpgradeSubsystem>();
+	if (!UpgradeSubsystem)
+	{
+		return;
+	}
+	UpgradeSubsystem->RequestUpgrade(true, BoundRowName);
+}
