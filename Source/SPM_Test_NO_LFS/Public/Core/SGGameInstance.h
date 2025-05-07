3c8159ee9f81655a76e4c68486754b8533367678
xWormx <carl_johan6@hotmail.com>
2025-05-07 22:55:18 +0200

    ADD - HUDGrapple och en animation för när man kan hooka

--- Source/SPM_Test_NO_LFS/Public/Core/SGGameInstance.h
+++ Source/SPM_Test_NO_LFS/Public/Core/SGGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Gear/Grapple/SGHUDGrapple.h"
 #include "SGGameInstance.generated.h"
 
 class USGTerminalWidget;
@@ -16,10 +17,16 @@ class SPM_TEST_NO_LFS_API USGGameInstance : public UGameInstance
 	GENERATED_BODY()
 public:
 	virtual void Init() override;
+
 	void SetTerminalWidget(USGTerminalWidget* InWidget) { HUDTerminal = InWidget; }
+	void SetHUDGrapple(USGHUDGrapple* InHUDGrapple) { HUDGrapple = InHUDGrapple; }
 	USGTerminalWidget* GetTerminalWidget() { return HUDTerminal; }
+	USGHUDGrapple* GetHUDGrapple() {	return HUDGrapple; }
 private:
 	UPROPERTY(VisibleAnywhere, Category = UPROPERTY)
 	USGTerminalWidget* HUDTerminal;
+
+	UPROPERTY(VisibleAnywhere, Category = UPROPERTY)
+	USGHUDGrapple* HUDGrapple;
 
 };
