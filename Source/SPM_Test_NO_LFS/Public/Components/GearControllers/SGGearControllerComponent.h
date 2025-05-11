4d2e34e19a4079e3794edeb97c18a73b4e228cce
Emma <emmarichterlarsson@gmail.com>
2025-05-11 18:15:15 +0200

    ADD - GearContollerComponent (basklass)

---
+++ Source/SPM_Test_NO_LFS/Public/Components/GearControllers/SGGearControllerComponent.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SGGearControllerComponent.generated.h"
+
+UCLASS(Abstract, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent, RestrictedToClasses = "Character"))
+class SPM_TEST_NO_LFS_API USGGearControllerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	USGGearControllerComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void InitGear(){};
+
+	virtual void SetUpInputs();
+
+	UFUNCTION()
+	virtual void Fire(){}
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "UProperty - Input", meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* InputMapping;
+
+	UPROPERTY()
+	TWeakObjectPtr<ACharacter> OwnerCharacter;
+
+};
+
