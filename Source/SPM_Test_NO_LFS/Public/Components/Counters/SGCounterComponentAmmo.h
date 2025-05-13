3128c73ed70c8c24990167d873cb860e26ba9f62
Emma <emmarichterlarsson@gmail.com>
2025-05-13 18:21:35 +0200

    ADD / EDIT - CounterComponent för Ammo + anpassat Ammo-pickupen för användning av CounterComponent

---
+++ Source/SPM_Test_NO_LFS/Public/Components/Counters/SGCounterComponentAmmo.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SGCounterComponent.h"
+#include "SGCounterComponentAmmo.generated.h"
+
+class ASGPlayerCharacter;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SPM_TEST_NO_LFS_API USGCounterComponentAmmo : public USGCounterComponent
+{
+	GENERATED_BODY()
+
+public:
+	USGCounterComponentAmmo();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void ProcessPickup(AActor* Pickup) override;
+
+private:
+	TWeakObjectPtr<ASGPlayerCharacter> OwnerPlayer;
+};
