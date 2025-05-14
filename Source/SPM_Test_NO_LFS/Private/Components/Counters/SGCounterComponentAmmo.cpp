a688f11a44279c467eda5e356b5653d49214494b
GMBruther <leonard.svensson@gmail.com>
2025-05-14 09:27:20 +0200

    fixade error
    la till en fil som gjorde att man inte kunde komma in

--- Source/SPM_Test_NO_LFS/Private/Components/Counters/SGCounterComponentAmmo.cpp
+++ Source/SPM_Test_NO_LFS/Private/Components/Counters/SGCounterComponentAmmo.cpp
@@ -1,7 +1,7 @@
 ﻿#include "Components/Counters/SGCounterComponentAmmo.h"
 
 #include "Gear/Weapons/SGGun.h"
-#include "Pickups/SGPickUpAmmo.h"
+#include "Pickups/SGAmmoPickUp.h"
 #include "Player/SGPlayerCharacter.h"
 
 USGCounterComponentAmmo::USGCounterComponentAmmo()
@@ -17,17 +17,17 @@ void USGCounterComponentAmmo::BeginPlay()
 
 void USGCounterComponentAmmo::ProcessPickup(AActor* Pickup)
 {
-	ASGPickUpAmmo* Ammo = Cast<ASGPickUpAmmo>(Pickup);
+	ASGAmmoPickUp* Ammo = Cast<ASGAmmoPickUp>(Pickup);
 	if (!Ammo || !OwnerPlayer.IsValid())
 	{
 		return;
 	}
 	for (ASGGun* Gun : OwnerPlayer->GetGuns())
 	{
-		if (!Ammo->IsCompatibleWithGun(Gun->GetClass()))
-		{
-			continue;
-		}
+		//if (!Ammo->IsCompatibleWithGun(Gun->GetClass()))
+		//{
+		//	continue;
+		//}
 
 		Gun->AddAmmo(Ammo->GetPickupValue());
 		break; //Ta bort denna om flera vapen kan använda samma ammo
