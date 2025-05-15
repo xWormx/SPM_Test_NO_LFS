374935cf6d86021b0026a1f7e59c336eddab6af4
basirghani <144438561+basirghani@users.noreply.github.com>
2025-05-15 20:50:07 +0200

    Added: Nav Link Class for Ground Enemies

---
+++ Source/SPM_Test_NO_LFS/Private/Enemies/Components/SGEnemyNavLink.cpp
@@ -0,0 +1,40 @@
+﻿
+#include "Enemies/Components/SGEnemyNavLink.h"
+
+#include "Enemies/AI/SGAIControllerEnemyBase.h"
+#include "GameFramework/Character.h"
+#include "Navigation/NavLinkProxy.h"
+
+ASGEnemyNavLink::ASGEnemyNavLink()
+{
+	
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+
+void ASGEnemyNavLink::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OnSmartLinkReached.AddDynamic(this, &ASGEnemyNavLink::OnSmartLinkReachedHandler);
+	
+}
+
+void ASGEnemyNavLink::OnSmartLinkReachedHandler(AActor* MovingActor, const FVector& DestinationPoint)
+{
+	ACharacter* Character = Cast<ACharacter>(MovingActor);
+
+	if (Character)
+	{
+		ASGAIControllerEnemyBase* AIController = Cast<ASGAIControllerEnemyBase>(Character->GetController());
+
+		if (AIController)
+		{
+			//AIController->JumpTowardsTarget(DestinationPoint);
+		}
+	}
+	
+}
+
+
+
