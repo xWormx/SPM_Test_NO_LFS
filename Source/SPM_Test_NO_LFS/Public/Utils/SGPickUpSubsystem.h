352cab93fcb2134a4390559ec499c6a6fca110f0
Emma <emmarichterlarsson@gmail.com>
2025-05-17 17:53:31 +0200

    ADD - EnemyDropManager byts till ett subsystem för pickups

---
+++ Source/SPM_Test_NO_LFS/Public/Utils/SGPickUpSubsystem.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SGPickUpSubsystem.generated.h"
+
+class USGObjectPoolSubsystem;
+class ASGEnemyCharacter;
+struct FEnemyDropInfo;
+
+UCLASS()
+class SPM_TEST_NO_LFS_API USGPickUpSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+public:
+	void DropItem(ASGEnemyCharacter* EnemyCharacter) const;
+
+private:
+	TMap<TSubclassOf<ASGEnemyCharacter>, TArray<FEnemyDropInfo>> EnemyDropMap;
+
+};
