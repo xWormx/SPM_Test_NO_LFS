03ccd6a92ea420952a8171277fedc93c0fa25010
basirghani <144438561+basirghani@users.noreply.github.com>
2025-05-20 22:24:35 +0200

    Added: Started implementing saving system

---
+++ Source/SPM_Test_NO_LFS/Private/SaveGame/USGISaveGame.cpp
@@ -0,0 +1,2 @@
+#include "SaveGame/USGISaveGame.h"
+
---
+++ Source/SPM_Test_NO_LFS/Public/SaveGame/SGSaveGame.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "USGISaveGame.h"
+#include "SGSaveGame.generated.h"
+
+
+class USGHealthComponent;
+class ASGGun;
+
+USTRUCT(Blueprintable)
+struct FPlayerStats
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere)
+	FTransform PlayerTransform;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<ASGGun> PlayerGun;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<USGHealthComponent> PlayerHealth;
+};
+
+UCLASS(Blueprintable)
+class SPM_TEST_NO_LFS_API USGSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+
+public:
+	UPROPERTY(EditAnywhere)
+	FPlayerStats PlayerStats;
+
+	/*UFUNCTION(BlueprintCallable)
+	virtual void LoadGameFromSlot();*/
+
+	/*virtual void LoadGameData_Implementation(bool Async) override;
+
+	virtual void SaveGameData_Implementation(bool Async) override;
+
+	virtual FPlayerStats GetGameData_Implementation() override;*/
+};
