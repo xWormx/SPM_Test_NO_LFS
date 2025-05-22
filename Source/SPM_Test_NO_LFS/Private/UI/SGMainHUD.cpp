e33b5bbd7a182a1160b9d4e7f2400b959be9afcc
Emma <emmarichterlarsson@gmail.com>
2025-05-22 09:54:25 +0200

    ADD - AHUD-klass

---
+++ Source/SPM_Test_NO_LFS/Private/UI/SGMainHUD.cpp
@@ -0,0 +1,82 @@
+﻿#include "UI/SGMainHUD.h"
+#include "SPM_Test_NO_LFS.h"
+#include "Core/SGGameInstance.h"
+#include "UI/Widgets/SGHUDGrapple.h"
+#include "Gear/Grapple/SGGrapplingHook.h"
+#include "Objectives/SGTerminalWidget.h"
+#include "Objectives/SGObjectiveToolTipWidget.h"
+
+void ASGMainHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//TerminalWidget = CreateAndAddToViewPort<USGTerminalWidget>(TerminalClass);
+	//ObjectiveWidget = CreateAndAddToViewPort<USGObjectiveToolTipWidget>(ObjectiveClass);
+
+	GrappleCrossHairWidget = CreateAndAddToViewPort<USGHUDGrapple>(GrappleCrossHairClass);
+	HUDWidget = CreateAndAddToViewPort<UUserWidget>(HUDClass);
+
+	/*if (USGGameInstance* GameInstance = Cast<USGGameInstance>(GetGameInstance()))
+	{
+		TerminalWidget->SetVisibility(ESlateVisibility::Hidden);
+		GameInstance->SetTerminalWidget(TerminalWidget.Get());
+		EMMA_LOG(Log, TEXT("❤️TerminalWidget: %s"), *GameInstance->GetTerminalWidget()->GetName());
+
+		ObjectiveWidget->SetVisibility(ESlateVisibility::Collapsed);
+		GameInstance->SetObjectiveTooltipWidget(ObjectiveWidget.Get());
+		EMMA_LOG(Log, TEXT("❤️ObjectiveToolTipWidget: %s"), *GameInstance->GetObjectiveTooltipWidget()->GetName());
+	}*/
+}
+
+void ASGMainHUD::BindToGrappleEvents(ASGGrapplingHook* GrapplingHook)
+{
+	if (!GrapplingHook)
+	{
+		return;
+	}
+	GrapplingHook->OnFireGrapple.AddDynamic(this, &ASGMainHUD::OnFireGrapple);
+	GrapplingHook->OnCanGrapple.AddDynamic(this, &ASGMainHUD::OnCanGrapple);
+	GrapplingHook->OnBeginCooldown.AddDynamic(this, &ASGMainHUD::OnBeginGrappleCooldown);
+}
+
+void ASGMainHUD::OnBeginGrappleCooldown(FTimerHandle& GrappleTimerHandle)
+{
+	const float TimeRemaining = GetWorld()->GetTimerManager().GetTimerRemaining(GrappleTimerHandle);
+	EMMA_LOG(Log, TEXT("❤️Grapple Cooldown: %f"), TimeRemaining);
+}
+
+void ASGMainHUD::OnFireGrapple()
+{
+	EMMA_LOG(Log, TEXT("❤️Grapple Fired!"));
+}
+
+void ASGMainHUD::OnCanGrapple(bool bCanGrapple)
+{
+	if (!bCanGrapple)
+	{
+		GrappleCrossHairWidget->PlayValidTargetAnimation();
+	}
+}
+
+template <typename T>
+T* ASGMainHUD::CreateAndAddToViewPort(const TSubclassOf<T>& WidgetClass, const bool Add)
+{
+	if (!WidgetClass)
+	{
+		return nullptr;
+	}
+
+	T* Widget = CreateWidget<T>(GetWorld(), WidgetClass);
+
+	if (!Widget)
+	{
+		return nullptr;
+	}
+
+	if (Add)
+	{
+		Widget->AddToViewport();
+	}
+
+	return Widget;
+}
---
+++ Source/SPM_Test_NO_LFS/Public/UI/SGMainHUD.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SGMainHUD.generated.h"
+
+class USGWeaponsHUD;
+class USGTerminalWidget;
+class USGObjectiveToolTipWidget;
+class ASGGrapplingHook;
+class USGHUDGrapple;
+
+UCLASS()
+class SPM_TEST_NO_LFS_API ASGMainHUD : public AHUD
+{
+	GENERATED_BODY()
+public:
+	virtual void BeginPlay() override;
+
+	template<typename T = UUserWidget>
+	T* CreateAndAddToViewPort(const TSubclassOf<T>& WidgetClass, bool Add = true);
+
+	void BindToGrappleEvents(ASGGrapplingHook* GrapplingHook);
+
+protected:
+	//------------GEAR------------
+	//----WEAPONS
+	UPROPERTY(EditAnywhere, Category ="Uproperty - HUD")
+	TSubclassOf<USGWeaponsHUD> WeaponsClass;
+
+	UPROPERTY(BlueprintReadOnly, Category ="Uproperty - HUD", meta=(BindWidget))
+	TWeakObjectPtr<USGWeaponsHUD> WeaponsWidget;
+
+	//----GRAPPLING
+	UPROPERTY(EditAnywhere, Category ="Uproperty - HUD")
+	TSubclassOf<USGHUDGrapple> GrappleCrossHairClass;
+
+	UPROPERTY(BlueprintReadOnly, Category ="Uproperty - HUD", meta=(BindWidget))
+	TWeakObjectPtr<USGHUDGrapple> GrappleCrossHairWidget;
+
+	//------------OBJECTIVES------------
+	//----MISSION INFO
+	UPROPERTY(EditAnywhere, Category ="Uproperty - HUD")
+	TSubclassOf<USGObjectiveToolTipWidget> ObjectiveClass;
+
+	UPROPERTY(BlueprintReadOnly, Category ="Uproperty - HUD", meta=(BindWidget))
+	TWeakObjectPtr<USGObjectiveToolTipWidget> ObjectiveWidget;
+
+	//----TERMINAL
+	UPROPERTY(EditAnywhere, Category ="Uproperty - HUD")
+	TSubclassOf<USGTerminalWidget> TerminalClass;
+
+	UPROPERTY(BlueprintReadOnly, Category ="Uproperty - HUD", meta=(BindWidget))
+	TWeakObjectPtr<USGTerminalWidget> TerminalWidget;
+
+	//------------HUD------------
+	//----CONTAINER
+	UPROPERTY(EditDefaultsOnly, Category = "Uproperty - HUD")
+	TSubclassOf<UUserWidget> HUDClass;
+
+	UPROPERTY(BlueprintReadOnly, Category ="Uproperty - HUD", meta=(BindWidget))
+	TWeakObjectPtr<UUserWidget> HUDWidget;
+
+public:
+	UFUNCTION()
+	void OnBeginGrappleCooldown(FTimerHandle& GrappleTimerHandle);
+
+	UFUNCTION()
+	void OnFireGrapple();
+
+	UFUNCTION()
+	void OnCanGrapple(bool bCanGrapple);
+};
