88f74b5f66f9301fcffd633ec7581a2c2cf831bc
Emma <emmarichterlarsson@gmail.com>
2025-05-22 19:06:05 +0200

    ADD - WeaponEntry (en rad för varje vapen)

---
+++ Source/SPM_Test_NO_LFS/Private/UI/Widgets/SGWeaponEntry.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Widgets/SGWeaponEntry.h"
+
+void USGWeaponEntry::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
