480957893ee8e8a59b3ca8954758afc6a6121145
Emma <emmarichterlarsson@gmail.com>
2025-05-22 10:07:28 +0200

    EDIT - flyttat SGHUDGrapple till UI-mapp

--- Source/SPM_Test_NO_LFS/Private/UI/Widgets/SGHUDGrapple.cpp
+++ Source/SPM_Test_NO_LFS/Private/UI/Widgets/SGHUDGrapple.cpp
@@ -1,15 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Gear/Grapple/SGHUDGrapple.h"
+#include "UI/Widgets/SGHUDGrapple.h"
 
 void USGHUDGrapple::NativeConstruct()
 {
 	Super::NativeConstruct();
-	if (ValidTargetAnimation)
-	{
-		
-	}
 }
 
 void USGHUDGrapple::PlayValidTargetAnimation()
--- Source/SPM_Test_NO_LFS/Public/UI/Widgets/SGHUDGrapple.h
+++ Source/SPM_Test_NO_LFS/Public/UI/Widgets/SGHUDGrapple.h
@@ -1,30 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "SGHUDGrapple.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class SPM_TEST_NO_LFS_API USGHUDGrapple : public UUserWidget
 {
 	GENERATED_BODY()
+
 public:
- void NativeConstruct() override;
+	virtual void NativeConstruct() override;
 	void PlayValidTargetAnimation();
-	
+
 protected:
 	UPROPERTY(BlueprintReadWrite, Transient, meta=(BindWidgetAnim))
 	UWidgetAnimation* ValidTargetAnimation;
 
 private:
-	
 	UFUNCTION()
 	void OnValidTargetAnimationEnd();
-	
+
 	FWidgetAnimationDynamicEvent ValidTargetAnimationEnd;
 };
