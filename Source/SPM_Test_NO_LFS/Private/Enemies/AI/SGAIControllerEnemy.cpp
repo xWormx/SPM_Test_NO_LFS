a3b5ae1a785f1b1a3d8aa68d59224e77866add76
basirghani <144438561+basirghani@users.noreply.github.com>
2025-05-28 22:44:12 +0200

    Added: Testing Behavior Trees

---
+++ Source/SPM_Test_NO_LFS/Private/Enemies/AI/SGAIControllerEnemy.cpp
@@ -0,0 +1,45 @@
+﻿#include "Enemies/AI/SGAIControllerEnemy.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ASGAIControllerEnemy::ASGAIControllerEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ASGAIControllerEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (BehaviorTree)
+	{
+		RunBehaviorTree(BehaviorTree);
+		AttackTarget = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		AttackTargetLocation = AttackTarget->GetActorLocation();
+		AttackTargetLocationInAir = FVector(AttackTarget->GetActorLocation().X, AttackTarget->GetActorLocation().Y, GetPawn()->GetActorLocation().Z);
+		GetBlackboardComponent()->SetValueAsObject(TEXT("AttackTarget"), AttackTarget);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("AttackTargetLocation"), AttackTargetLocation);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("AttackTargetLocationInAir"), AttackTargetLocationInAir);
+	}
+}
+
+void ASGAIControllerEnemy::SetInitialValues()
+{
+}
+
+void ASGAIControllerEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (BehaviorTree)
+	{
+		AttackTargetLocation = AttackTarget->GetActorLocation();
+		AttackTargetLocationInAir = FVector(AttackTarget->GetActorLocation().X, AttackTarget->GetActorLocation().Y, GetPawn()->GetActorLocation().Z);
+		GetBlackboardComponent()->SetValueAsObject(TEXT("AttackTarget"), AttackTarget);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("AttackTargetLocation"), AttackTargetLocation);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("AttackTargetLocationInAir"), AttackTargetLocationInAir);
+	}
+}
+
