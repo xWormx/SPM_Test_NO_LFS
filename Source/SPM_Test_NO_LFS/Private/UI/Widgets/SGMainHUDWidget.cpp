8274b0bf6bfad73c034c62fe1af380db73bd18a6
Emma <86224710+emrila@users.noreply.github.com>
2025-05-31 23:41:26 +0200

    ADD - HUD widget-klass

---
+++ Source/SPM_Test_NO_LFS/Private/UI/Widgets/SGMainHUDWidget.cpp
@@ -0,0 +1,70 @@
+﻿
+
+#include "UI/Widgets/SGMainHUDWidget.h"
+
+#include "Core/SGGameInstance.h"
+#include "UI/Widgets/SGHUDGrapple.h"
+#include "UI/Widgets/SGWeaponsHUD.h"
+#include "UI/Widgets/SGDifficultyBarWidget.h"
+#include "Components/Widget.h"
+
+
+void USGMainHUDWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	HasFirstQuestStarted = false;
+
+	PauseAndHide();
+	DifficultyWidget->PauseDifficultyBar(true);
+	DifficultyWidget->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void USGMainHUDWidget::PauseAndHide()
+{
+	if (WeaponsWidget.IsValid())
+	{
+		WeaponsWidget->SetVisibility(ESlateVisibility::Collapsed);
+	}
+	if (GrappleHookWidget.IsValid())
+	{
+		GrappleHookWidget->SetVisibility(ESlateVisibility::Collapsed);
+	}	
+	if (DifficultyWidget.IsValid() && HasFirstQuestStarted)
+	{
+		DifficultyWidget->PauseDifficultyBar(true);
+		DifficultyWidget->SetVisibility(ESlateVisibility::Collapsed);
+	}
+
+	GetGameInstance<USGGameInstance>()->GetObjectiveTooltipWidget()->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void USGMainHUDWidget::PlayAndShow()
+{
+	if (WeaponsWidget.IsValid())
+	{
+		WeaponsWidget->SetVisibility(ESlateVisibility::Visible);
+	}
+	if (GrappleHookWidget.IsValid())
+	{
+		GrappleHookWidget->SetVisibility(ESlateVisibility::Visible);
+	}
+	
+	if (DifficultyWidget.IsValid() && HasFirstQuestStarted)
+	{
+		DifficultyWidget->PauseDifficultyBar(false);
+		DifficultyWidget->SetVisibility(ESlateVisibility::Visible);
+	}
+	
+	GetGameInstance<USGGameInstance>()->GetObjectiveTooltipWidget()->SetVisibility(ESlateVisibility::Visible);
+}
+
+void USGMainHUDWidget::StartDifficultyBar()
+{
+	HasFirstQuestStarted = true;
+	if (DifficultyWidget.IsValid())
+	{
+		DifficultyWidget->SetVisibility(ESlateVisibility::Visible);
+		DifficultyWidget->PauseDifficultyBar(false);
+	}
+}
---
+++ Source/SPM_Test_NO_LFS/Public/UI/Widgets/SGMainHUDWidget.h
@@ -0,0 +1,48 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SGMainHUDWidget.generated.h"
+
+class USGDifficultyBarWidget;
+class USGHUDGrapple;
+class USGWeaponsHUD;
+
+UCLASS()
+class SPM_TEST_NO_LFS_API USGMainHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+	void PauseAndHide();
+	void PlayAndShow();
+	
+	USGWeaponsHUD* GetWeaponsWidget() const
+	{
+		return WeaponsWidget.Get();
+	}
+	USGHUDGrapple* GetGrappleHookWidget() const
+	{
+		return GrappleHookWidget.Get();
+	}
+	USGDifficultyBarWidget* GetDifficultyWidget() const
+	{
+		return DifficultyWidget.Get();
+	}
+
+	void StartDifficultyBar();
+protected:
+	UPROPERTY(BlueprintReadOnly, Category ="Uproperty - HUD", meta=(BindWidget))
+	TWeakObjectPtr<USGWeaponsHUD> WeaponsWidget;
+
+	UPROPERTY(BlueprintReadOnly, Category ="Uproperty - HUD", meta=(BindWidget))
+	TWeakObjectPtr<USGHUDGrapple> GrappleHookWidget;
+
+	UPROPERTY(BlueprintReadOnly, Category ="Uproperty - HUD", meta=(BindWidget))
+	TWeakObjectPtr<USGDifficultyBarWidget> DifficultyWidget;
+
+private:
+	bool HasFirstQuestStarted = false;
+};
