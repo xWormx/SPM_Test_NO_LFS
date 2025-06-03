8297bf43193c76ff8eaec009aa344bff2179d910
Emma <emmarichterlarsson@gmail.com>
2025-06-03 10:17:53 +0200

    ADD - factory/helper-klass för konstruktion av återkommande widgetdata

---
+++ Source/SPM_Test_NO_LFS/Public/UI/SGWidgetFactory.cpp
@@ -0,0 +1,59 @@
+﻿#include "SGWidgetFactory.h"
+#include "SlateWidgets/SWidgetData/StyleSetData.h"
+
+FButtonData SGWidgetFactory::MenuButton(const FText& ButtonText, const FOnClicked& OnClicked, const bool bPrimary)
+{
+	FName ButtonStyleName = bPrimary ? StyleNames::MenuPrimaryButton() : StyleNames::MenuButton();
+	FName TextStyleName = bPrimary ? StyleNames::MenuPrimaryButtonText() : StyleNames::MenuButtonText();
+
+	FButtonData Button = FButtonData();
+	Button.OnClicked = OnClicked;
+	Button.ButtonStyle = FStyleSetData::Get().GetWidgetStyle<FButtonStyle>(ButtonStyleName);
+	Button.TextData = CreateTextData(ButtonText, TextStyleName);
+
+	return Button;
+}
+
+FTextData SGWidgetFactory::CreateTextData(const FText& Text, const FName StyleName)
+{
+	FTextData TextData;
+	TextData.Text = Text;
+	TextData.TextStyle = FStyleSetData::Get().GetWidgetStyle<FTextBlockStyle>(StyleName);
+	return TextData;
+}
+
+FTextData SGWidgetFactory::CreateHeader(const FText& Text, bool bMenu)
+{
+	FName StyleName = StyleNames::MenuHeaderText();
+	FTextData TextData;
+	TextData.Text = Text;
+	TextData.TextStyle = FStyleSetData::Get().GetWidgetStyle<FTextBlockStyle>(StyleName);
+	return TextData;
+}
+
+FBackgroundData SGWidgetFactory::Background(const FSlateColor& BackgroundColor, const FAlignmentData& AlignmentData)
+{
+	FBackgroundData BackgroundData;
+	BackgroundData.BackgroundColor = BackgroundColor;
+	BackgroundData.BackgroundAlignment = AlignmentData;
+	return BackgroundData;
+}
+
+FButtonGroupData SGWidgetFactory::ButtonGroup(const TArray<FButtonData>& ButtonDataArray, const EOrientation Orientation, const FAlignmentData& AlignmentData)
+{
+	FButtonGroupData ButtonGroupData;
+	ButtonGroupData.ButtonDataArray = ButtonDataArray;
+	ButtonGroupData.Orientation = Orientation;
+	ButtonGroupData.Alignment = AlignmentData;
+	return ButtonGroupData;
+}
+
+FMenuData SGWidgetFactory::CreateMenuData(const FText& TextData, const FButtonGroupData& ButtonGroupData, const FBackgroundData& BackgroundData, const FAlignmentData MenuAlignmentData)
+{
+	FMenuData MenuData;
+	MenuData.HeaderElement =  CreateHeader(TextData);
+	MenuData.ButtonGroupElement = ButtonGroupData;
+	MenuData.BackgroundData = BackgroundData;
+	MenuData.ElementAlignment = MenuAlignmentData;
+	return MenuData;
+}
---
+++ Source/SPM_Test_NO_LFS/Public/UI/SGWidgetFactory.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "SlateWidgets/SWidgetData/WidgetConstructionData.h"
+
+
+class SGWidgetFactory
+{
+public:
+
+	static FButtonData MenuButton(const FText& ButtonText, const FOnClicked& OnClicked, bool bPrimary = false);
+
+	static FTextData CreateTextData(const FText& Text, const FName StyleName);
+
+	static FTextData CreateHeader(const FText& Text, bool bMenu = true);
+
+	static FBackgroundData Background(const FSlateColor& BackgroundColor, const FAlignmentData& AlignmentData);
+
+	static FButtonGroupData ButtonGroup(const TArray<FButtonData>& ButtonDataArray,const EOrientation Orientation, const FAlignmentData& AlignmentData);
+
+	static FMenuData CreateMenuData(const FText& TextData, const FButtonGroupData& ButtonGroupData,const FBackgroundData& BackgroundData,FAlignmentData MenuAlignmentData);
+};
