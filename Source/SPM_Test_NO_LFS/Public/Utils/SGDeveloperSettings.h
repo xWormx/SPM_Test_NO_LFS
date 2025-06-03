e9571c07cc802f617c6e15acbee251ab9ca07825
Emma <emmarichterlarsson@gmail.com>
2025-06-03 10:16:53 +0200

    EDIT - tabllerna för strings och styling för UI/menyer läggs i DeveloperSettings

--- Source/SPM_Test_NO_LFS/Public/Utils/SGDeveloperSettings.h
+++ Source/SPM_Test_NO_LFS/Public/Utils/SGDeveloperSettings.h
@@ -20,10 +20,15 @@ public:
 	UPROPERTY(Config, EditAnywhere, Category="Enemy Drops Data Table", meta=(AllowedClasses="/Script/Engine.DataTable"))
 	FSoftObjectPath EnemyDropsDataTable;
 
-	UPROPERTY(Config, EditAnywhere, Category="Object Pool Data Table", meta=(AllowedClasses="/Script/Engine.DataTable", ToolTip="Vilka actors som ska initeras vid start"))
-	FSoftObjectPath ObjectPoolDataTable;
+	/*UPROPERTY(Config, EditAnywhere, Category="Object Pool Data Table", meta=(AllowedClasses="/Script/Engine.DataTable", ToolTip="Vilka actors som ska initeras vid start"))
+	FSoftObjectPath ObjectPoolDataTable;*/
 
 	UPROPERTY(Config, EditAnywhere, Category="Weapon UI Data Table", meta=(AllowedClasses="/Script/Engine.DataTable"))
 	FSoftObjectPath WeaponUIDataTable;
 
+	UPROPERTY(Config, EditAnywhere, Category="UI Styling", meta=(AllowedClasses="/Script/Engine.DataTable"))
+	FSoftObjectPath StylingThemeDataTable;
+
+	UPROPERTY(Config, EditAnywhere, Category="UI String Table", meta=(AllowedClasses="/Script/Engine.StringTable"))
+	FSoftObjectPath UIStringTable;
 };
