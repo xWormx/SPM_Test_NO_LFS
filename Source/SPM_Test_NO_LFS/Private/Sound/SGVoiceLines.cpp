c7fe16afe4c5c8fa550dbcfc0e0b2f63efcebdb0
Joel Wendt <joel.wendt90@gmail.com>
2025-06-03 21:28:37 +0200

    Added: Några funktioner att binda till delegates

--- Source/SPM_Test_NO_LFS/Private/Sound/SGVoiceLines.cpp
+++ Source/SPM_Test_NO_LFS/Private/Sound/SGVoiceLines.cpp
@@ -50,8 +50,7 @@ void ASGVoiceLines::PlaySound(USoundBase* Sound)
 	if (AudioComponent->IsPlaying()) return;
 	if (!CooldownMap.Contains(Sound) || CooldownMap[Sound] > 0.f) return;
 	
-	CooldownMap[Sound] = 5.f;
-	
+	CooldownMap[Sound] = 10.f;
 	
 	AudioComponent->SetSound(Sound);
 	AudioComponent->Play();
@@ -63,8 +62,17 @@ void ASGVoiceLines::BindDelegateHandlers() const
 	
 }
 
-
-void ASGVoiceLines::PlayFluffCue(ASGEnemyCharacter* Enemy)
+void ASGVoiceLines::Voice_Fluff(ASGEnemyCharacter* Enemy)
 {
 	PlaySound(Sounds[0]);
+}
+
+void ASGVoiceLines::Voice_Reload(int32 GunIndex, ASGGun* Gun)
+{
+	PlaySound(Sounds[1]);
+}
+
+void ASGVoiceLines::Voice_FindTerminal()
+{
+	PlaySound(Sounds[2]);
 }
--- Source/SPM_Test_NO_LFS/Public/Sound/SGVoiceLines.h
+++ Source/SPM_Test_NO_LFS/Public/Sound/SGVoiceLines.h
@@ -30,11 +30,16 @@ private:
 	
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="editor",meta=(AllowPrivateAccess = "true"))
 	TArray<USoundBase*> Sounds;
-
-	// Delegate handlers
+	
 	void BindDelegateHandlers() const;
 
 public:
 	UFUNCTION()
-	void PlayFluffCue(class ASGEnemyCharacter* Enemy);
+	void Voice_Fluff(class ASGEnemyCharacter* Enemy);
+
+	UFUNCTION()
+	void Voice_Reload(int32 GunIndex, class ASGGun* Gun);
+
+	UFUNCTION()
+	void Voice_FindTerminal();
 };
