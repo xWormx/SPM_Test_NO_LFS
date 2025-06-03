b84526734d03fc2f34e544d5201201d33d58fd76
Emma <emmarichterlarsson@gmail.com>
2025-06-03 10:14:56 +0200

    ADD - Styling av menyer (färg, form, font) kan göras i datatabell
    Så man inte behöver recompile varje gång

---
+++ Source/SPM_Test_NO_LFS/Public/UI/SlateWidgets/SWidgetData/StylingTheme.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "WidgetConstructionData.h"
+#include "StylingTheme.generated.h"
+
+USTRUCT()
+struct FStylingTheme : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Text Elements")
+	FTextBlockStyle Header;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Text Elements")
+	FTextBlockStyle Description;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Text Elements")
+	FTextBlockStyle PrimaryButtonText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Text Elements")
+	FTextBlockStyle SecondaryButtonText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Buttons")
+	FButtonStyle PrimaryButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Buttons")
+	FButtonStyle SecondaryButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Background")
+	FBackgroundData StartMenuBackground;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Background")
+	FBackgroundData PauseMenuBackground;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Background")
+	FBackgroundData GameOverMenuBackground;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Background")
+	FBackgroundData VictoryMenuBackground;
+};
