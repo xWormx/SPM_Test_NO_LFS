4f3428f0fa402a37c4130bd223e5525a5f3cbbde
basirghani <144438561+basirghani@users.noreply.github.com>
2025-06-06 22:53:50 +0200

    Fixed: Enemy Rotating towards player with interpolation instead of directly

---
+++ Source/SPM_Test_NO_LFS/Private/Enemies/BTServices/SGBTService_SetIsFacingTarget.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemies/BTServices/SGBTService_SetIsFacingTarget.h"
+
+#include "SPM_Test_NO_LFS.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Enemies/AI/SGAIControllerEnemy.h"
+
+USGBTService_SetIsFacingTarget::USGBTService_SetIsFacingTarget()
+{
+	NodeName = TEXT("Set Is Facing Target");
+}
+
+void USGBTService_SetIsFacingTarget::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	ASGAIControllerEnemy* AIController = Cast<ASGAIControllerEnemy>(OwnerComp.GetAIOwner());
+
+	if (!AIController)
+	{
+		BASIR_LOG(Error, TEXT("Set Is Facing Target, AIController is Null"));
+	}
+
+	const bool bIsFacingTarget = AIController->IsFacingTarget();
+
+	AIController->GetBlackboardComponent()->SetValueAsBool(GetSelectedBlackboardKey(), bIsFacingTarget);
+}
---
+++ Source/SPM_Test_NO_LFS/Public/Enemies/BTServices/SGBTService_SetIsFacingTarget.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "SGBTService_SetIsFacingTarget.generated.h"
+
+
+UCLASS()
+class SPM_TEST_NO_LFS_API USGBTService_SetIsFacingTarget : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	USGBTService_SetIsFacingTarget();
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
