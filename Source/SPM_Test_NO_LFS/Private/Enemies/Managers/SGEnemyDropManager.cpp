b9eba616cd0b6ebfb305277e167c0c271bc85de8
Emma <emmarichterlarsson@gmail.com>
2025-06-10 21:19:06 +0200

    REMOVE / EDIT - Gamla Actor-managers för enemy drops och object pool

--- Source/SPM_Test_NO_LFS/Private/Enemies/Managers/SGEnemyDropManager.cpp
+++
@@ -1,3 +0,0 @@
-﻿#include "Enemies/Managers/SGEnemyDropManager.h"
-
-//TODO: Ta bort actor från alla levels, dess BP och slutligen ta bort klassen
--- Source/SPM_Test_NO_LFS/Private/Utils/SGUtilObjectPoolManager.cpp
+++
@@ -1,2 +0,0 @@
-﻿#include "Utils/SGUtilObjectPoolManager.h"
-//TODO: Ta bort actor från alla levels, dess BP och slutligen ta bort klassen
